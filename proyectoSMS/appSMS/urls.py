90b0bc4c102cd1fe2953961d7551e48c2ed5bcb6
Pedro Rueda <pedrorueda.develop@gmail.com>
2024-10-12 14:27:52 +0200

    hasta aqui el socket funciona y en el listado de usuario se puede ver al logeado

--- proyectoSMS/appSMS/urls.py
+++ proyectoSMS/appSMS/urls.py
@@ -9,7 +9,4 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('chat/<str:username>/', views.chat_privado, name='chat_privado'),
-    # path('chat/', views.chat_privado, name='chat'),
-    # path('<str:username>/', views.chat_privado, name='room'),
-    # path('chat/<str:chatname>/', views.chat_privado, name='chat_privado'), sirve de guia.
 ]
