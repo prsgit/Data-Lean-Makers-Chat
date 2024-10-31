3cd86fced347e2fa5c14b93cbeea32b648213243
Pedro Rueda <pedrorueda.develop@gmail.com>
2024-10-31 17:02:45 +0100

    str añadido en GroupMessage en el models.py , lo demás sigue igual que en el commit anterior

--- proyectoSMS/appSMS/models.py
+++ proyectoSMS/appSMS/models.py
@@ -19,6 +19,9 @@ class GroupMessage(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.sender} to {self.group.name}: {self.content[:50]}'
+
 class Message(models.Model):
     room_name = models.CharField(max_length=255)
     sender = models.ForeignKey(
